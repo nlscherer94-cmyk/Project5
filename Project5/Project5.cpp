8b210e4c4d5a041b16d4baf740c2aabdbdb7a45b
Nicscherer <Nlscherer94@gmail.com>
2025-11-17 18:33:46 -0500

    Testing complete.

--- Project5/Project5.cpp
+++ Project5/Project5.cpp
@@ -1,42 +1,35 @@
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    int startOrganisms;
-    double avgdailyIncrease;
-    int days;
-    //Calulate starting number of organisms
-    cout << "What is the starting number of organisms? ";
-    cin >> startOrganisms;
-    while (startOrganisms < 2) {
-        cout << "\nInvalid input, must be above 2.\nPlease try again: ";
-        cin >> startOrganisms;
-    }
-    //Calulate the percentage increase each day
-    cout << endl << "What is the average daily population increase as a percentage (Ex. = 5 for 5%)? ";
-    cin >> avgdailyIncrease;
-    while (avgdailyIncrease < 0) {
-        cout << "\nInvalid input, must be a positive number.\nPlease try again: ";
-        cin >> avgdailyIncrease;
-    }
-    //Calulate the amount of days they will multiply
-    cout << endl << "What is the amount of days these organisms will multiply? ";
-    cin >> days;
-    while (days < 1) {
-        cout << "\nInvalid input, must be a number greater than 1.\nPlease try again: ";
-        cin >> days;
-    }
-    //Turns user input into a percentage
-    double rateofgrowth = avgdailyIncrease / 100;
-    //Output a chart of each day that displays the Day and Organisms as they increase
-    cout << fixed << setprecision(2);
-    cout << "Day" << "   " << "Organisms" << endl;
-    for (int day = 1; day <= days; day++) {
-        cout << day << "     " << startOrganisms << endl;
-        double increase = startOrganisms * rateofgrowth;
-        startOrganisms = startOrganisms + increase;
-    }
+	srand(time(0));
+	//Random number 
+	int randomNumber = rand() % 100 + 1;
+
+	int guess;
+	int guessCount = 0;
+	//output to user
+	cout << "****GUESSING GAME****" << endl;
+	cout << "I am thinking of a number between 1 and 100" << endl;
+	cout << "Can you guess what number I am thinking of?" << endl;
+
+	do {
+		cout << "\nEnter your guess: ";
+		cin >> guess;
+		guessCount++;
+		//tells user whether they are too high or too low
+		if (guess > randomNumber) {
+			cout << "\nToo high, try again!" << endl;
+		}
+		else if (guess < randomNumber) {
+			cout << "\nToo low, try again!" << endl;
+		}
+	} while (guess != randomNumber);//tells user their guess count
+	cout << "\nYOU GUESSED IT IN " << guessCount << " GUESSES!" << endl;
+	return 0;
 }
